9e32f698219f9f5110af7e797620ac444d084012
유아영 <ahyoung.db@gmail.com>
2020-02-19 17:51:34 +0900

    todolist

---
+++ 0219/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import CreateForm from "./components/CreateForm";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+  id = 1;
+
+  state = {
+    todos: []
+  };
+
+  // 인자값으로 text를 받고
+  // todos 배열을 가져온 뒤 값이 변화할 때마다
+  // todos에 새로운 배열을 추가함.
+  handleInsert = (text) => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.concat({
+        id: this.id++,
+        text,
+        checked: false
+      })
+    });
+  };
+
+  // 인자값으로 id를 받고
+  // todos배열에 map 메소드를 사용하여 돌면서
+  // 만약 현재 id값과 배열의 id값이 같으면
+  // 스프레드 연산자 사용하여 배열을 한번 복사한뒤 체크값을 반대로 바꿔줌
+  // 아니면 그냥 리턴함
+  handleToggle = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            checked: !todo.checked
+          };
+        }
+        return todo;
+      })
+    });
+  };
+
+  // 인자값으로 id를 받고
+  // todos배열에서 현재 id와 같은 배열을 제외한 나머지를 리턴하여 todos배열에 넣음
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter((todo) => todo.id !== id)
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h3>TODO LIST</h3>
+        <CreateForm onInsert={this.handleInsert} />
+        <TodoList
+          onToggle={this.handleToggle}
+          todos={this.state.todos}
+          onRemove={this.handleRemove}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 0219/src/components/TodoItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./TodoItem.css";
+
+class TodoItem extends Component {
+  render() {
+    const { todo, onToggle, onRemove } = this.props;
+
+    return (
+      <div
+        // "" = `` 같다 ``는 안에 변수를 넣어주고 싶을 때 쓰는것
+        className={`TodoItem ${todo.checked && "active"}`}
+        onClick={() => onToggle(todo.id)}
+      >
+        <div className="check">&#10004;</div>
+        <div
+          className="remove"
+          onClick={(e) => {
+            // 이벤트가 부모태그로 전파되는것을 막아주는것.
+            e.stopPropagation();
+            onRemove(todo.id);
+          }}
+        >
+          [remove]
+        </div>
+        <div className="text">{todo.text}</div>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
