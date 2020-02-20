ef9891c8b7750b9fbd2b247915071c6585b9e743
유아영 <ahyoung.db@gmail.com>
2020-02-20 11:44:26 +0900

    0215 update

---
+++ 0215/src/components/CreateForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./CreateForm.css";
+
+class CreateForm extends Component {
+  state = {
+    input: ""
+  };
+
+  // input의 value값을 가져온뒤
+  // 변화할때마다 input에 value값을 넣어줌
+  handleChange = (e) => {
+    const { value } = e.target;
+
+    this.setState({
+      input: value
+    });
+  };
+
+  // 부모컴포넌트 App에서 받아온 props onInsert를 사용해서 text에 현재 input값을 받아온 뒤 배열로 뱉어냄
+  // 그다음 input값 다시 비워줌
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onInsert(this.state.input);
+    this.setState({
+      input: ""
+    });
+  };
+  render() {
+    const { input } = this.state;
+
+    return (
+      <div className="CreateForm">
+        {/* 폼으로 보낼 때 handleSubmit 함수 실행 */}
+        <form className="form_container" onSubmit={this.handleSubmit}>
+          <input
+            placeholder="something to do?"
+            value={input}
+            onChange={this.handleChange}
+          />
+          <button type="submit">추가</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateForm;
---
+++ 0215/src/components/TodoItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./TodoItem.css";
+
+class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.todo !== nextProps.todo) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  render() {
+    const { todo, onToggle, onRemove } = this.props;
+
+    return (
+      <div
+        // "" = `` 같음 ``는 안에 변수를 넣어주고 싶을 때 쓰는것
+        className={`TodoItem ${todo.checked && "active"}`}
+        onClick={() => onToggle(todo.id)}
+      >
+        <div className="check">&#10004;</div>
+        <div
+          className="remove"
+          onClick={(e) => {
+            // 이벤트가 부모태그로 전파되는것을 막아주는것.
+            e.stopPropagation();
+            onRemove(todo.id);
+          }}
+        >
+          [remove]
+        </div>
+        <div className="text">{todo.text}</div>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
