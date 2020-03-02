8cbb6e98f62a19580024f55244ba80825d506042
유아영 <ahyoung.db@gmail.com>
2020-03-02 15:16:28 +0900

    review async

--- 0222_ver03/src/App.js
+++ 0222_ver03/src/App.js
@@ -14,6 +14,33 @@ class App extends Component {
     });
   };
 
+  handleAsyncClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/"
+      );
+      this.setState({
+        data: response.data
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  handlePromise = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(1);
+        resolve();
+      }, 1000);
+    });
+  };
+
+  handleAsync = async () => {
+    await this.handlePromise();
+    console.log(2);
+  };
+
   render() {
     const { data } = this.state;
 
@@ -21,7 +48,21 @@ class App extends Component {
       <div>
         {/* promise : 비동기를 동기적으로*/}
 
+        <button
+          onClick={() => {
+            this.handlePromise().then(() => {
+              console.log(2);
+            });
+          }}
+        >
+          click
+        </button>
+
+        <button onClick={this.handleAsync}>async click</button>
+        <br />
+
         <button onClick={this.handleClick}>데이터 불러오기</button>
+        <button onClick={this.handleAsyncClick}>async로 데이터 불러오기</button>
         <ul>
           {data &&
             data.map((item) => {
