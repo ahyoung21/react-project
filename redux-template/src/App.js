a58b79f7bf7b0a856e7ba75965949eae38d87dfd
유아영 <ahyoung.db@gmail.com>
2020-03-11 17:07:39 +0900

    modify app

--- redux-template/src/App.js
+++ redux-template/src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 // import Counter from './components/Counter';
 import CounterContainer from './containers/CounterContainer';
-import ColorList from './components/ColorList';
 // import ColorSquare from './components/ColorSquare';
 import ColorSquareContainer from './containers/ColorSquareContainer';
+// import ColorList from './components/ColorList';
+import ColorListContainer from './containers/ColorListContainer';
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
       <div className="App">
         <CounterContainer />
         <ColorSquareContainer />
-        <ColorList />
+        <ColorListContainer />
       </div>
     );
   }
