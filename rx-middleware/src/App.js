1ca6be880073e1c1d68e7fdeefe5fd1077ca22ff
유아영 <ahyoung.db@gmail.com>
2020-03-14 13:59:54 +0900

    redux middleware

---
+++ rx-middleware/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { changeInput, insert } from './store/modules/form';
+import * as FormActions from './store/modules/form';
+import { bindActionCreators } from 'redux';
+class App extends Component {
+  handleChange = e => {
+    const { value } = e.target;
+    this.props.changeInput(value);
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { formActions, insert, input } = this.props;
+
+    formActions.insertAsync(input);
+    formActions.changeInput('');
+  };
+
+  render() {
+    const { input, list } = this.props;
+    return (
+      <div>
+        <form action="">
+          <input value={input} onChange={this.handleChange} />
+          <button onClick={this.handleSubmit}>입력</button>
+        </form>
+        {list.map(item => {
+          return <div key={item.id}>{item.text}</div>;
+        })}
+      </div>
+    );
+  }
+}
+const mapDispatchToProps = dispatch => ({
+  formActions: bindActionCreators(FormActions, dispatch)
+});
+
+export default connect(
+  ({ form: { input, list } }) => ({
+    input,
+    list
+  }),
+  mapDispatchToProps
+)(App);
---
+++ rx-middleware/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './store/modules';
+import { Provider } from 'react-redux';
+import middleware from './middleware';
+import ReduxThunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(middleware, ReduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
